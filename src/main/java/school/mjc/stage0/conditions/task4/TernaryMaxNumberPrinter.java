fa22be4ceb1da5279a90e9372457cc9bf40e12c3
rkamildev <rkamil.int@gmail.com>
2023-07-18 01:35:55 +0500

    TernaryOperator

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result;
+        result = (first >= second && first >= third) ?
+                first : (second >= first && second >= third) ?
+                    second : (third >= second && third >= first) ?
+                        third : 0;
+        System.out.println(result);
     }
 }
